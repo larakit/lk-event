35b3684001479a35e30173e34431c2b7d8f4f12a
Alexey Berdnikov <aberdnikov@gmail.com>
2016-08-04 17:41:04 +0500

    fixed

---
+++ src/Event/Event.php
@@ -0,0 +1,76 @@
+<?php
+namespace Larakit\Event;
+
+class Event {
+
+    protected static $dispatcher = null;
+
+    static function dispatcher() {
+        if(is_null(self::$dispatcher)) {
+            self::$dispatcher = new \sfEventDispatcher();
+        }
+
+        return self::$dispatcher;
+    }
+
+    /**
+     * Навешиваем слушателя на событие
+     *
+     * @param $event_name
+     * @param $callback
+     */
+    static function listener($event_name, $callback) {
+        self::dispatcher()
+            ->connect($event_name, $callback);
+    }
+
+    /**
+     * Уведомляем приложение о событии
+     *
+     * @param       $event_name
+     * @param array $parameters
+     * @param null  $subject
+     */
+    static function notify($event_name, $parameters = [], $subject = null) {
+        $event = new \sfEvent($subject, $event_name, $parameters);
+        self::dispatcher()
+            ->notify($event);
+    }
+
+    /**
+     * Уведомляем приложение о событии, ждем запуска хотя бы одного слушателя
+     * Возвращаем логическое значение - был ли запущен хот я бы один слушатель
+     *
+     * @param       $event_name
+     * @param array $parameters
+     * @param null  $subject
+     *
+     * @return bool
+     */
+    static function notifyUntil($event_name, $parameters = [], $subject = null) {
+        $event = new \sfEvent($subject, $event_name, $parameters);
+        $event = self::dispatcher()
+            ->notifyUntil($event);
+
+        return $event->isProcessed();
+    }
+
+    /**
+     * Фильтр по событию
+     *
+     * @param       $event_name
+     * @param null  $default
+     * @param array $parameters
+     * @param null  $subject
+     *
+     * @return mixed
+     */
+    static function filter($event_name, $default = null, $parameters = [], $subject = null) {
+        $event = new \sfEvent($subject, $event_name, $parameters);
+        self::dispatcher()
+            ->filter($event, $default);
+
+        return $event->getReturnValue();
+    }
+
+}
